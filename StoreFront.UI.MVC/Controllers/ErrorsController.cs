7fe09334ead2821f0b96c5441af608d7b4557032
Sam Negus <sam.negus@outlook.com>
2024-02-24 14:19:31 -0600

    Fixed AJAX delete functionality

---
+++ StoreFront.UI.MVC/Controllers/ErrorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public IActionResult Status(int id)
+        {
+            (int id, string message) error; //Tuple -> New version of an anonymous object.
+            
+            error.id = id;
+            error.message = id switch
+            {
+                404 => "Page Not Found",
+                500 => "Internal Server Error",
+                _ => "Unknown Error"
+            }; 
+
+            return View(error);
+        }
+    }
+}
